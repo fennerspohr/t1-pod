a1b572f13e8314c3840ceecdd892acee2e4d2e7a
fennerspohr <isadorafennerspohr@gmail.com>
2024-11-01 18:06:04 -0300

    leitura do arquivo

--- bucket-selecao.c
+++ bucket-selecao.c
@@ -1,14 +1,32 @@
-//
-// Created by evergreenis on 01/11/24.
-//
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, unsigned int *argv[])){
-for( i=0; i<argc; i++ )
-    {
-        printf("%u\n", (unsigned int) [i]); //Do whatever you want with argv[i]
-    }
+void read_ints (const char* file_name, int* valores)
+{
+    FILE* file = fopen (file_name, "r");
+    int i = 0;
+    int in = 0;
 
-return 0
+    fscanf (file, "%d", &i);
+    while (!feof (file))
+    {
+        valores[in] = i;
+        fscanf (file, "%d", &i);
+        in++;
+    }
+    fclose (file);
+}
+
+int main(int argc){
+
+const char file_name[11] = "output.txt";
+int valores[argc];
+
+read_ints(file_name, (int *) &valores);
+
+for(int i = 0; i <argc; i++){
+    printf("%d\n", valores[i]);
+}
+
+return 0;
 }
