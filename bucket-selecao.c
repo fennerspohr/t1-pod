15c33a485197c5417aaa2064659cf7c8259f6b9d
fennerspohr <isadorafennerspohr@gmail.com>
2024-11-01 17:15:35 -0300

    initial commit

---
+++ bucket-selecao.c
@@ -0,0 +1,14 @@
+//
+// Created by evergreenis on 01/11/24.
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, unsigned int *argv[])){
+for( i=0; i<argc; i++ )
+    {
+        printf("%u\n", (unsigned int) [i]); //Do whatever you want with argv[i]
+    }
+
+return 0
+}
