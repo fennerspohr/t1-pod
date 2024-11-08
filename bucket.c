f11464facd8d36415c04456b9c783bbf98e539c1
fennerspohr <isadorafennerspohr@gmail.com>
2024-11-08 20:02:33 -0300

    heap sort

--- bucket.c
+++ bucket.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include "quicksort.h"
 #define maxNum 1001 // Maior numero do range + 1
-#define numBaldes 10 // Numero de baldes
-#define tam 1000 // Quantidade de numeros a serem ordenados
+#define numBaldes 100 // Numero de baldes
+#define tam 150000 // Quantidade de numeros a serem ordenados
 
 typedef struct balde
 {
@@ -27,6 +26,58 @@ void read_ints (const char* file_name, int valores[])
         in++;
     }
     fclose (file);
+}
+void heapify(int arr[], int n, int i) {
+
+    // Initialize largest as root
+    int largest = i;
+
+    // left index = 2*i + 1
+    int l = 2 * i + 1;
+
+    // right index = 2*i + 2
+    int r = 2 * i + 2;
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest]) {
+        largest = l;
+    }
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest]) {
+        largest = r;
+    }
+
+    // If largest is not root
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// Main function to do heap sort
+void heapSort(int arr[], int n) {
+
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i > 0; i--) {
+
+        // Move current root to end
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        // Call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
 }
 
 void selectionSort(int arr[]) {
@@ -141,7 +192,8 @@ void ordenaBalde(Balde *baldes, int tipo)
 {
     if(tipo==1){ //quick sort
         for (int i = 0; i < numBaldes; i++){
-            initial_quick(baldes[i].vet, 0, tam-1);
+//            initial_quick(baldes[i].vet, 0, tam-1);
+            heapSort(baldes[i].vet, tam);
         }
     }
     else if(tipo ==2){ //selection sort
@@ -169,7 +221,7 @@ int main(){
     read_ints(file_name,  valores); //le os valores do arquivo
 
     int tipo;
-    printf("Qual tipo de sort deseja utilizar?\n1)Quick sort\n2)Selection sort\n3)Merge sort\n");
+    printf("Qual tipo de sort deseja utilizar?\n1)Heap sort\n2)Selection sort\n3)Merge sort\n4)Insertion sort\n");
     scanf("%d", &tipo);
     // alocacao dos baldes
     Balde* baldes = malloc(tam*sizeof(Balde)); //inicializados em 0
